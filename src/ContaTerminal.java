ef8dd241c46036254f0a87146a2f827a0d56beda
jordidbmf <jordi@zamplex.com.br>
2023-08-29 11:24:11 -0300

    Desafio realizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Obter pela scanner os valores digitados no terminal
+
+        System.out.println("Por favor, digite o seu Nome e um Sobrenome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int conta = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        //Exibir a mensagem conta criada
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
